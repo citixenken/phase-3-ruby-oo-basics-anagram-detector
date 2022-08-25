45895b4fcac04a65cf4691e650e99f493388f3cd
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-25 22:33:04 +0300

    COMPLETED: anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+    attr_accessor :array_list
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array_list)
+        array_list.filter {|word_occurrence| word_occurrence.chars.sort == @word.chars.sort}
+    end
+end
+
+#verify
+listen = Anagram.new("allergy")
+listen.match(%w[gallery ballerina regally clergy largely leading])
